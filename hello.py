e3c3781fe999a1301610051e470c0ac4379498d8
yug2005 <yug.sahu@mail.utoronto.ca>
2024-09-24 00:29:38 -0400

    Add templates and complete activity 1.3

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+Bootstrap(app)
+Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
